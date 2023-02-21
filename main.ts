74a56eed72d4c6cf2ffa5c6856e935e3a96836b9
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-02-21 17:12:28 +0100

    Erste Zeit verändert, RGB rot

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 basic.showString("hi!")
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
-    basic.pause(1000)
+    basic.pause(500)
     basic.showIcon(IconNames.No)
     basic.pause(1000)
+})
+basic.forever(function () {
+    basic.setLedColor(0xff0000)
 })
